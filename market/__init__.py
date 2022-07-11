d5e7b9fc4448e3451187a1251f4c1763bb532195
amrg-410 <rahulgandhiaids@smvec.ac.in>
2022-07-11 22:51:31 +0530

    Register Form Creation

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '793f0563db6e1716dc0dfb8f'
 db = SQLAlchemy(app)
 
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -1,5 +1,10 @@
+from cProfile import label
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 
 class RegisterForm(FlaskForm):
-    username = StringField(Label='username')
+    username = StringField(label='User Name:')
+    email_address = StringField(label="Email Address:")
+    password1 = PasswordField(label="Password:")
+    password2 = PasswordField(label="Confirm Password:")
+    submit = SubmitField(label="Create Account")
