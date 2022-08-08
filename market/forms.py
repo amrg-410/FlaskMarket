c719d899e33aea894f5f67d5a4764b7b1c43feab
amrg-410 <rahulgandhiaids@smvec.ac.in>
2022-08-08 10:38:19 +0530

    User name update

--- market/forms.py
+++ market/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:', validators = [Length(min=5,max=10), DataRequired()])
+    username = StringField(label="User Name:", validators = [Length(min=5,max=10), DataRequired()])
     email_address = StringField(label="Email Address:", validators=[Email(), DataRequired()])
     password1 = PasswordField(label="Password:", validators = [Length(min=6), DataRequired()])
     password2 = PasswordField(label="Confirm Password:", validators = [EqualTo('password1'), DataRequired()])
